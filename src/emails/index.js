0b5f226df369b4fdc236b806e92afe4e96ee0854
Kasun Vithanage <alankasun@gmail.com>
2021-03-05 14:21:57 +0530

    emails

---
+++ src/emails/index.js
@@ -0,0 +1,52 @@
+const qs = require('querystring');
+const log = require('../log');
+const config = require('../config');
+
+const { mailQueue } = require('../services/queue');
+/**
+ * Add mail to queue
+ *
+ * @param {string} name - name of the mail
+ * @param {object} options - options to send mail
+ * @param {number} [priority=undefined] - priority of mail(lower the higher)
+ */
+async function addToMailQueue(name, options, priority = undefined) {
+  try {
+    await mailQueue.add(name, options, {
+      attempts: 5,
+      backoff: {
+        type: 'exponential',
+        delay: 5 * 1000,
+      },
+      priority,
+      removeOnComplete: true,
+    });
+  } catch (error) {
+    log.error('Error sending mail', error);
+  }
+}
+
+exports.addToMailQueue = addToMailQueue;
+
+/**
+ * Send user verification mail
+ *
+ * @param {User} user - user
+ * @param {string} token - token
+ */
+async function sendUserVerificationMail(user, token) {
+  log.info(`Sending user account verification mail to email:${user.email}`, {
+    mail: 'user-verification',
+  });
+
+  await addToMailQueue('user-verification', {
+    template: 'user-verification',
+    to: user.email,
+    locals: {
+      user,
+      link: `${config.redirects.accountVerification}?${qs.stringify({ token })}`,
+    },
+  });
+}
+
+exports.sendUserVerificationMail = sendUserVerificationMail;
