5a9f7230411f5c75b791ec7f07796ffa3dd6329c
Kasun Vithanage <alankasun@gmail.com>
2021-03-07 17:03:10 +0530

    reset controller

---
+++ src/controllers/user/common/sendPasswordReset.js
@@ -0,0 +1,108 @@
+const { customAlphabet } = require('nanoid/async');
+
+const { v4: uuidv4 } = require('uuid');
+const { addDays } = require('date-fns');
+const APIError = require('../../../errors/APIError');
+// eslint-disable-next-line no-unused-vars
+const User = require('../../../models/user.model');
+const { sendPasswordResetMail } = require('../../../emails');
+const { SMS } = require('../../../sms');
+const { sendSMS } = require('../../../sms/sender');
+const PasswordResetRequest = require('../../../models/passwordResetRequest.model');
+const { OTPSession, OTP_SCOPE_PASSWORD_RESET } = require('../../../otp');
+
+const fourDigitRandomId = customAlphabet('1234567890', 4);
+
+const TEN_MINUTES_IN_SEC = 10 * 60;
+
+/**
+ * send with code
+ *
+ * @param {User} userDAO
+ * @returns
+ */
+async function sendEmailWithCode(userDAO) {
+  const userId = userDAO.id;
+  // clear old verifications and resend email
+  await PasswordResetRequest.destroy({
+    where: {
+      userId,
+    },
+  });
+
+  const verificationId = uuidv4();
+  await PasswordResetRequest.create({
+    userId,
+    method: 'email',
+    verificationId,
+    expiresAt: addDays(new Date(), 1),
+  });
+
+  await sendPasswordResetMail(userDAO, verificationId);
+
+  return {
+    method: 'email',
+  };
+}
+
+/**
+ * send with code
+ *
+ * @param {User} userDAO
+ * @returns
+ */
+async function sendSMSCode(userDAO) {
+  const { id: userId, phone } = userDAO;
+  const prevOtp = await OTPSession.GetSession(phone, OTP_SCOPE_PASSWORD_RESET);
+
+  if (prevOtp) {
+    // we have already valid activation
+    return {
+      requestId: prevOtp.requestId,
+      method: 'phone',
+    };
+  }
+
+  const requestId = uuidv4();
+  const otpCode = await fourDigitRandomId();
+
+  const otpSession = new OTPSession(
+    phone,
+    userId,
+    requestId,
+    otpCode,
+    OTP_SCOPE_PASSWORD_RESET,
+    TEN_MINUTES_IN_SEC,
+  );
+  await otpSession.save();
+
+  const sms = SMS.CreateOTPMessage(userDAO.phone, otpCode);
+  await sendSMS(sms);
+
+  return {
+    requestId,
+    method: 'phone',
+  };
+}
+
+/**
+ * send activation code
+ *
+ * @param {User} userDAO
+ * @returns
+ */
+async function sendPasswordReset(userDAO) {
+  if (userDAO.email) {
+    return sendEmailWithCode(userDAO);
+  }
+
+  if (userDAO.phone) {
+    return sendSMSCode(userDAO);
+  }
+
+  throw new APIError('Unknown provider');
+}
+
+module.exports = {
+  sendPasswordReset,
+};
--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,7 +1,9 @@
 const { register } = require('./register');
 const { resendActicationCode } = require('./resendActivationCode');
+const { sendPasswordReset } = require('./sendPasswordReset');
 
 module.exports = {
   register,
   resendActicationCode,
+  sendPasswordReset,
 };
