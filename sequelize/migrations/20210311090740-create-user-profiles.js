31962aaf36dbbab764fc38368f25b269bb665e00
Kasun Vithanage <alankasun@gmail.com>
2021-03-11 15:06:03 +0530

    models + migrations

---
+++ sequelize/migrations/20210311090740-create-user-profiles.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('userProfiles', {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    firstName: {
+      type: Sequelize.STRING,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+    },
+    bio: {
+      type: Sequelize.TEXT,
+    },
+    dateOfBirth: {
+      type: Sequelize.DATE,
+    },
+    avatar: {
+      type: Sequelize.TEXT,
+    },
+    genderId: {
+      type: Sequelize.UUID,
+      references: {
+        model: 'genders',
+        key: 'id',
+      },
+      onDelete: 'SET NULL',
+    },
+    userId: {
+      type: Sequelize.UUID,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+    },
+  }),
+
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('userProfiles'),
+};
