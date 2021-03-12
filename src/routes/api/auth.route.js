e1d1ec17f8ce5facfb12e14f4ec7d28ae3df6068
Kasun Vithanage <alankasun@gmail.com>
2021-03-12 20:15:48 +0530

    refactor

--- src/routes/api/auth.route.js
+++ src/routes/api/auth.route.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const { validate } = require('express-validation');
-const authController = require('../../controllers/auth');
+const controller = require('../../controllers/auth');
 const validation = require('../../validations/auth.validation');
 
-router.post('/login', validate(validation.login), authController.login);
-router.post('/login/token', validate(validation.loginToken), authController.loginRefreshToken);
-router.post('/verify/account', validate(validation.verifyUser), authController.verifyAccount);
-router.post('/verify/otp', validate(validation.verifyOtp), authController.verifyOtpRequest);
+router.post('/login', validate(validation.login), controller.login);
+router.post('/login/token', validate(validation.loginToken), controller.loginRefreshToken);
+router.post('/verify/account', validate(validation.verifyUser), controller.verifyAccount);
+router.post('/verify/otp', validate(validation.verifyOtp), controller.verifyOtpRequest);
 
 module.exports = router;
--- src/routes/api/profile.route.js
+++ src/routes/api/profile.route.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 const { validate } = require('express-validation');
 const auth = require('../../middlewares/auth');
-const userController = require('../../controllers/profile');
+const controller = require('../../controllers/profile');
 const validation = require('../../validations/profile.validation');
 
-router.patch('/me', validate(validation.updateProfile), auth(), userController.updateProfile);
+router.patch('/me', validate(validation.updateProfile), auth(), controller.updateProfile);
 
 module.exports = router;
