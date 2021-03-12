22a2cf77665d572f9e11e59331325248e4bc0ca1
Kasun Vithanage <alankasun@gmail.com>
2021-03-12 19:29:52 +0530

    interests

---
+++ sequelize/migrations/20210312135238-seed-default-interests.js
@@ -0,0 +1,59 @@
+const { v4: uuid } = require('uuid');
+
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('interests', [
+      {
+        id: uuid(),
+        title: 'Sports',
+      },
+      {
+        id: uuid(),
+        title: 'Trees',
+      },
+      {
+        id: uuid(),
+        title: 'Tea',
+      },
+      {
+        id: uuid(),
+        title: 'Coffee',
+      },
+      {
+        id: uuid(),
+        title: 'Yoga',
+      },
+      {
+        id: uuid(),
+        title: 'Art',
+      },
+      {
+        id: uuid(),
+        title: 'Dance',
+      },
+      {
+        id: uuid(),
+        title: 'Writing',
+      },
+      {
+        id: uuid(),
+        title: 'Reading',
+      },
+      {
+        id: uuid(),
+        title: 'Blogging',
+      },
+      {
+        id: uuid(),
+        title: 'Music',
+      },
+    ], {
+
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('People', null, {});
+  },
+};
