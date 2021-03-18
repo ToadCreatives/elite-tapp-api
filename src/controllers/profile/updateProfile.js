7ab3778f649f2617e8fc2435f42c8f780a3ca3b4
Kasun Vithanage <alankasun@gmail.com>
2021-03-18 22:55:59 +0530

    few improvements

--- src/controllers/profile/updateProfile.js
+++ src/controllers/profile/updateProfile.js
@@ -1,25 +1,6 @@
 const httpStatus = require('http-status');
 const { pick } = require('lodash');
-const UserProfile = require('../../models/userProfile.model');
-
-/**
- * Get or create profile for user
- *
- * @param {string} userId - user id
- * @returns
- */
-async function getOrCreateUserProfile(userId) {
-  let userProfile = await UserProfile.findOne({
-    where: {
-      userId,
-    },
-  });
-  if (!userProfile) {
-    userProfile = UserProfile.build({ userId });
-  }
-
-  return userProfile;
-}
+const { getOrCreateUserProfile } = require('./common/profile');
 
 /**
  * Returns a safe object with safe to update params
