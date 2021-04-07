0f5aa5ee4cc2c376f3c039b79108763a7c11d0ae
Kasun Vithanage <alankasun@gmail.com>
2021-04-07 14:25:50 +0530

    link prefix

--- src/utils/userLinkHelper.js
+++ src/utils/userLinkHelper.js
@@ -7,104 +7,127 @@ const providers = {
   // free social
   facebook: {
     pathPrefix: 'https://facebook.com',
+    isUrl: true,
     tier: Tiers.free,
   },
   twitter: {
     pathPrefix: 'https://twitter.com',
+    isUrl: true,
     tier: Tiers.free,
   },
   instagram: {
     pathPrefix: 'https://www.instagram.com',
+    isUrl: true,
     tier: Tiers.free,
   },
   pinterest: {
     pathPrefix: 'https://www.pinterest.com',
+    isUrl: true,
     tier: Tiers.free,
   },
   twitch: {
     pathPrefix: 'http://twitch.tv',
+    isUrl: true,
     tier: Tiers.free,
   },
   snapchat: {
     pathPrefix: 'https://www.snapchat.com/add',
+    isUrl: true,
     tier: Tiers.free,
   },
   linktree: {
     pathPrefix: 'https://linktr.ee',
+    isUrl: true,
     tier: Tiers.free,
   },
 
   // free contacts
   text: {
-    pathPrefix: null,
+    pathPrefix: 'tel:',
+    isUrl: false,
     tier: Tiers.free,
   },
   email: {
     pathPrefix: null,
+    isUrl: false,
     tier: Tiers.free,
   },
   whatsapp: {
     pathPrefix: null,
+    isUrl: false,
     tier: Tiers.free,
   },
   address: {
     pathPrefix: null,
+    isUrl: false,
     tier: Tiers.free,
   },
   facetime: {
     pathPrefix: null,
+    isUrl: true,
     tier: Tiers.free,
   },
   call: {
     pathPrefix: null,
+    isUrl: false,
     tier: Tiers.free,
   },
 
   // free music
   spotify: {
     pathPrefix: null,
+    isUrl: true,
     tier: Tiers.free,
   },
   applemusic: {
     pathPrefix: null,
+    isUrl: true,
     tier: Tiers.free,
   },
   soundcloud: {
     pathPrefix: 'https://soundcloud.com',
+    isUrl: true,
     tier: Tiers.free,
   },
 
   // free payments
   paypal: {
     pathPrefix: 'https://paypal.com',
+    isUrl: true,
     tier: Tiers.free,
   },
   venmo: {
     pathPrefix: 'https://venmo.com',
+    isUrl: true,
     tier: Tiers.free,
   },
   cashapp: {
     pathPrefix: 'https://cash.app',
+    isUrl: true,
     tier: Tiers.free,
   },
 
   // elite plus
   tiktok: {
     pathPrefix: 'https://tiktok.com',
+    isUrl: true,
     tier: Tiers.plus,
   },
   youtube: {
     pathPrefix: null,
+    isUrl: true,
     tier: Tiers.plus,
   },
   podcasts: {
     pathPrefix: null,
+    isUrl: true,
     tier: Tiers.plus,
   },
 
   // custom
   website: {
     pathPrefix: null,
+    isUrl: true,
     tier: Tiers.plus,
   },
 };
@@ -125,8 +148,11 @@ function getResourceUrl(provider, path) {
   }
 
   const providerData = providers[provider];
-  if (providerData) {
+  if (providerData.isUrl) {
     return urljoin(providerData.pathPrefix, path);
+  }
+  if (!providerData.isUrl && providerData.pathPrefix) {
+    return [providerData.pathPrefix, path].join('');
   }
 
   return path;
