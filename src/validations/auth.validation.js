933be801721fde34616235f10f7e73c7dcdc0cd8
Kasun Vithanage <alankasun@gmail.com>
2021-04-22 12:35:56 +0530

    remove login validation

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,15 +1,11 @@
 const Joi = require('joi');
-const { JOI_PHONE_SCHEMA, JOI_EMAIL_SCHEMA } = require('../utils/helpers');
 
 // User validation rules
 module.exports = {
   login: {
     body: Joi.object({
-      login: Joi.alternatives().try(
-        JOI_EMAIL_SCHEMA,
-        JOI_PHONE_SCHEMA,
-      ).required(),
-      password: Joi.string().min(6).max(128).required(),
+      login: Joi.string().required(),
+      password: Joi.string().required(),
     }),
   },
   loginToken: {
