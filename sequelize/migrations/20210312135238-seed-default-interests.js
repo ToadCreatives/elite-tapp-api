401e0005996662a74fdcdb9b11a2113953e7613f
Kasun Vithanage <alankasun@gmail.com>
2021-05-19 11:20:48 +0530

    update default

--- sequelize/migrations/20210312135238-seed-default-interests.js
+++ sequelize/migrations/20210312135238-seed-default-interests.js
@@ -1,60 +1,36 @@
 const { v4: uuid } = require('uuid');
 
+const interests = [
+  'Travel',
+  'Entrepreneurship',
+  'Adventure',
+  'Sports',
+  'Pets',
+  'Technology',
+  'Comedy',
+  'Art',
+  'Music',
+  'Outdoors',
+  'Influencer',
+  'Working Out',
+  'Movies',
+  'Concerts',
+  'Gaming',
+  'Mental Health',
+  'LGBTQ+',
+  'Food',
+  'Dancing',
+  'Reading',
+];
+
 /* eslint-disable no-unused-vars */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('interests', [
-      {
-        id: uuid(),
-        title: 'Sports',
-      },
-      {
-        id: uuid(),
-        title: 'Trees',
-      },
-      {
-        id: uuid(),
-        title: 'Tea',
-      },
-      {
-        id: uuid(),
-        title: 'Coffee',
-      },
-      {
-        id: uuid(),
-        title: 'Yoga',
-      },
-      {
-        id: uuid(),
-        title: 'Art',
-      },
-      {
-        id: uuid(),
-        title: 'Dance',
-      },
-      {
-        id: uuid(),
-        title: 'Writing',
-      },
-      {
-        id: uuid(),
-        title: 'Reading',
-      },
-      {
-        id: uuid(),
-        title: 'Blogging',
-      },
-      {
-        id: uuid(),
-        title: 'Music',
-      },
-      {
-        id: uuid(),
-        title: 'Games',
-      },
-    ], {
-
-    });
+    await queryInterface.bulkInsert(
+      'interests',
+      interests.map((interest) => ({ id: uuid(), title: interest })),
+      {},
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
