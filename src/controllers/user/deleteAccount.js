ef90ff92c1dbf2a661cc3cb0af89a8c21937631b
Kasun Vithanage <alankasun@gmail.com>
2021-05-31 00:45:47 +0530

    use where

--- src/controllers/user/deleteAccount.js
+++ src/controllers/user/deleteAccount.js
@@ -5,7 +5,7 @@ exports.deleteAccount = async (req, res, next) => {
   try {
     const { user } = req;
 
-    await User.destroy({ id: user.id });
+    await User.destroy({ where: { id: user.id } });
 
     return res.status(httpStatus.OK).json({ message: 'ok' });
   } catch (err) {
